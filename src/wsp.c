53f7c5eecfb7344fa6fa2563a83f89198e931ad8
OjaanIr <farnasrozaan7@gmail.com>
2022-01-24 15:18:33 +0700

    TODO: tambah comparison, laporan, readme, dan test

--- src/wsp.c
+++ src/wsp.c
@@ -3,49 +3,36 @@
 int main() {
     int rowPuzzle, totalWords;
     readFile(&rowPuzzle, &totalWords);
-
-    // for (int i = 0; i < rowPuzzle; i++)
-    // {
-    //     for (int j = 0; j < strlen(puzzle[0]); j++)
-    //     {
-    //         printf("%c ", puzzle[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-    
-    // printf("\n");
-
-    // for (int i = 0; i < totalWords; i++)
-    // {
-    //     printf("WORD: %s\n", word[i]);
-    // }
-    
     solve(rowPuzzle, totalWords);
     return 0;
 }
 
 void readFile(int *rowPuzzle, int *totalWords) {
     FILE *file; 
-    file = fopen("../test/test.txt", "r");
+    char fileName[20];
+    printf("Input file name (../test/namafile.txt): ");
+    scanf("%s", fileName);
+    file = fopen(fileName, "r");
 
     if (file == NULL)
     {
         printf("Error in opening file!!!\n");
+        exit(1);
     }
     else {
         char line[40], line2[40];
-        int i, rows=0, substractor;
+        int i, rows=0, cols;
 
         do
         {
             fgets(line,sizeof(line),file);
-            substractor = 0;
+            cols = 0;
             for (i = 0; i < strlen(line)-1; i++)
             {
                 if (line[i] != 32)
                 {
-                    puzzle[rows][i-substractor] = line[i];
-                    substractor++;
+                    puzzle[rows][cols] = line[i];
+                    cols++;
                 }
             }
             rows++;
@@ -75,63 +62,58 @@ void solve(int rowPuzzle, int totalWords) {
     clock_t time = clock();
     for (i = 0; i < totalWords; i++)
     {
+        printf("\n");
         for (j = 0; j < rowPuzzle; j++)
         {
             for (k = 0; k < colPuzzle; k++)
             {
                 wordLength = strlen(word[i]);
-                // printf("Check length: %d\n", checkLength(j, k+wordLength-1));
-                // printf("Search right: %d\n", searchRight(j, k, wordLength, i, &comparison));
-                if (checkLength(j-wordLength+1, k) == 1 && searchUp(j,k,wordLength,i,&comparison) == 1)
+                if (checkLength(j-wordLength+1, k)&& searchUp(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionUp(rowPuzzle, j, k, wordLength);
                 }
                 else if (checkLength(j-wordLength+1, k+wordLength-1) && searchUpRight(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionUpRight(rowPuzzle, j, k, wordLength);
                 }
                 else if (checkLength(j, k+wordLength-1) && searchRight(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionRight(rowPuzzle, j, k, wordLength);
                 }
                 else if (checkLength(j+wordLength-1, k+wordLength-1) && searchDownRight(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionDownRight(rowPuzzle, j, k, wordLength);
                 }
                 else if (checkLength(j+wordLength-1, k) && searchDown(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionDown(rowPuzzle, j, k, wordLength);
                 }
-                else if (checkLength(j-wordLength+1, k-wordLength+1) && searchDownLeft(j,k,wordLength,i,&comparison))
+                else if (checkLength(j+wordLength-1, k-wordLength+1) && searchDownLeft(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionDownLeft(rowPuzzle, j, k, wordLength);
                 }
                 else if (checkLength(j, k-wordLength+1) && searchLeft(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionLeft(rowPuzzle, j, k, wordLength);
                 }
                 else if (checkLength(j-wordLength+1, k-wordLength+1) && searchUpLeft(j,k,wordLength,i,&comparison))
                 {
-                    printf("%d. %s\n", i+1, word[i], comparison);
+                    printf("%d. %s\n", i+1, word[i]);
                     displaySolutionUpLeft(rowPuzzle, j, k, wordLength);
                 }   
-                // else
-                // {
-                //     printf("Word %s doesn't exist in the puzzle!!!\n", word[i]);
-                // }
             }
         }
     }
     time = clock() - time;
     double time_elapsed = ((double) time)/CLOCKS_PER_SEC;
-    printf("Time elapsed = %.3f seconds\n", time_elapsed);
+    printf("\nTime elapsed = %.3f seconds\n", time_elapsed);
 }
 
 int checkLength(int row, int col) {
@@ -330,6 +312,7 @@ void displaySolutionUp(int rowPuzzle, int row, int col, int wordLength) {
 }
 
 void displaySolutionUpRight(int rowPuzzle, int row, int col, int wordLength) {
+    int x = 0;
     int upLimit = row-wordLength+1;
     int rightLimit = col+wordLength-1;
     int colPuzzle = strlen(puzzle[0]);
@@ -337,9 +320,10 @@ void displaySolutionUpRight(int rowPuzzle, int row, int col, int wordLength) {
     {
         for (int j = 0; j < colPuzzle; j++)
         {
-            if (i >= upLimit && i <= row && j >= col && j <= rightLimit)
+            if (i == upLimit+x && i <= row && j >= col && j == rightLimit-x)
             {
                 printf("%c ", puzzle[i][j]);
+                x++;
             }
             else
             {
@@ -371,6 +355,7 @@ void displaySolutionRight(int rowPuzzle, int row, int col, int wordLength) {
 }
 
 void displaySolutionDownRight(int rowPuzzle, int row, int col, int wordLength) {
+    int x = 0;
     int lowLimit = row+wordLength-1;
     int rightLimit = col+wordLength-1;
     int colPuzzle = strlen(puzzle[0]);
@@ -378,9 +363,10 @@ void displaySolutionDownRight(int rowPuzzle, int row, int col, int wordLength) {
     {
         for (int j = 0; j < colPuzzle; j++)
         {
-            if (i >= row && i <= lowLimit && j >= col && j <= rightLimit)
+            if (i == row+x && i <= lowLimit && j == col+x && j <= rightLimit)
             {
                 printf("%c ", puzzle[i][j]);
+                x++;
             }
             else
             {
@@ -412,6 +398,7 @@ void displaySolutionDown(int rowPuzzle, int row, int col, int wordLength) {
 }
 
 void displaySolutionDownLeft(int rowPuzzle, int row, int col, int wordLength) {
+    int x = 0;
     int lowLimit = row+wordLength-1;
     int leftLimit = col-wordLength+1;
     int colPuzzle = strlen(puzzle[0]);
@@ -419,9 +406,10 @@ void displaySolutionDownLeft(int rowPuzzle, int row, int col, int wordLength) {
     {
         for (int j = 0; j < colPuzzle; j++)
         {
-            if (i >= row && i <= lowLimit && j >= leftLimit && j <= col)
+            if (i == row+x && i <= lowLimit && j >= leftLimit && j == col-x)
             {
                 printf("%c ", puzzle[i][j]);
+                x++;
             }
             else
             {
@@ -453,16 +441,18 @@ void displaySolutionLeft(int rowPuzzle, int row, int col, int wordLength) {
 }
 
 void displaySolutionUpLeft(int rowPuzzle, int row, int col, int wordLength) {
+    int x = 0;
     int upLimit = row-wordLength+1;
-    int leftLimit = row-wordLength+1;
+    int leftLimit = col-wordLength+1;
     int colPuzzle = strlen(puzzle[0]);
     for (int i = 0; i < rowPuzzle; i++)
     {
         for (int j = 0; j < colPuzzle; j++)
         {
-            if (i >= upLimit && i <= row && j >= leftLimit && j <= col)
+            if (i == upLimit+x && i <= row && j == leftLimit+x && j <= col)
             {
                 printf("%c ", puzzle[i][j]);
+                x++;
             }
             else
             {
@@ -473,4 +463,4 @@ void displaySolutionUpLeft(int rowPuzzle, int row, int col, int wordLength) {
     }
 }
 
-// TODO: tambah comparison, fix bug arah serong2
+// TODO: tambah comparison, laporan, dan readme
